1cfdcf34ea01b0deea30affad5b3db30f9d7301f
Tran, Tuan <tran@metsci.com>
2022-11-17 15:16:21 -0500

    Fixed errors

--- com/metsci/Hello.java
+++ com/metsci/Hello.java
@@ -1,9 +1,11 @@
+package com.metsci;
+
 import java.util.logging.Logger;
 
 class Hello {
   
   static Logger logger = Logger.getLogger(Hello.class.getName());
   public static void main(String [] args){
-    lopgger.info("Hi");
+    logger.info("Hi");
   }
 }
