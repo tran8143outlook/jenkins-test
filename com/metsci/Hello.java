9a7bfaf9acf96446e200bc79975329bf8d9e53c9
Tran, Tuan <tran@metsci.com>
2022-11-18 10:04:52 -0500

    good bye

--- com/metsci/Hello.java
+++ com/metsci/Hello.java
@@ -7,5 +7,6 @@ class Hello {
   static Logger logger = Logger.getLogger(Hello.class.getName());
   public static void main(String [] args){
     logger.info("Hi hi hi hi");
+    logger.info("good bye");
   }
 }
