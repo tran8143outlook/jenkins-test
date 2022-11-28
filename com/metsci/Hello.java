db776ca3d53eec86f424c249cb8bb201e90ea5be
Tran, Tuan <tran@metsci.com>
2022-11-28 09:41:58 -0500

    Branch A

--- com/metsci/Hello.java
+++ com/metsci/Hello.java
@@ -7,7 +7,7 @@ class Hello {
   static Logger logger = Logger.getLogger(Hello.class.getName());
   public static void main(String [] args){
 
-	  logger.info("Hi hi hi hi webhooks 12345");
+	  logger.info("Hi hi hi hi webhooks 12345 A");
 	      // b
   }
 }
